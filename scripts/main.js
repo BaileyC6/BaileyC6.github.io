3bab2be71d7c3bc687d5ed6871ed8c1a27681ce0
Bailey Cartusciello <baileycartusciello@gmail.com>
2022-10-18 22:55:20 -0600

    XIC button adds code

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,44 @@
-const instructions = document.querySelectorAll(".instruction-group .instruction");
-const newrungbutton = document.querySelector(".Rung")
+const instructions = document.querySelectorAll(".instruction-menu .instruction");
+const newrungbutton = document.querySelector(".NewRung")
 let NumOfRungs = 0;
 
-for (const instruction of instructions) {
-  instruction.addEventListener('click', addinstruction);
+// for (const instruction of instructions) {
+//   instruction.addEventListener('click', addinstruction);
+// }
+
+// function addinstruction() {
+//   // alert('added instruction');
+// }
+
+const XICelement = document.createElement('div.XICinstruction')
+
+document.querySelector('.instruction > .XIC').addEventListener('click', addXIC);
+
+function addXIC() {
+  test = new XIC();
+  document.querySelector('.Rung').innerText += test.structuredText;
 }
 
 newrungbutton.addEventListener('click', addnewrung);
 
-function addinstruction() {
-  alert('added instruction');
-}
-
 function addnewrung() {
   const addedrung = document.createElement('p');
-  addedrung.innerText = NumOfRungs + ' |-----------------------------|';
+  addedrung.className = 'Rung';
+  addedrung.innerText = `${NumOfRungs}`;
   document.querySelector('.Code').appendChild(addedrung);
   NumOfRungs ++;
+}
+
+class Instruction {
+  constructor (text, tagName = '?') {
+    this.structuredText = `${text}(${tagName})`;
+    this.tagName = tagName;
+  }
+}
+
+class XIC extends Instruction {
+  value = false;
+  constructor(tagName = '?') {
+    super('XIC', tagName);
+  }
 }
