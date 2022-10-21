09984d302eb5dd330fdd2d2b63838559b980c3c7
Bailey Cartusciello <baileycartusciello@gmail.com>
2022-10-20 22:25:08 -0600

    XIC add instruction code

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,58 @@
 const instructions = document.querySelectorAll(".instruction-menu .instruction");
-const newrungbutton = document.querySelector(".NewRung")
-let NumOfRungs = 0;
+
+let NumOfRungs = 1;
+
+var selectedRung;
+
+// document.querySelector('.Rung').addEventListener('click', SelectRung)
 
 // for (const instruction of instructions) {
 //   instruction.addEventListener('click', addinstruction);
 // }
 
-// function addinstruction() {
-//   // alert('added instruction');
-// }
+//I dont know if this should be a function or a class
+class inputDiv {
+  addedInstruction;
+  constructor(InstructionName, tagName = '?') {
+    const tagNameDiv = document.createElement('input');
+    tagNameDiv.className = 'TagName'
+    tagNameDiv.value = tagName;
+    const InputDiv = document.createElement('div');
+    InputDiv.className = 'Input';
+    InputDiv.appendChild(tagNameDiv);
 
-const XICelement = document.createElement('div.XICinstruction')
+    const instructionSymbol = document.createElement('div');
+
+    switch (InstructionName) {
+      case 'XIC':
+        const addedInstruction = new XIC(tagName);
+        instructionSymbol.className = 'XICInstruction';
+        instructionSymbol.innerText = 'XIC'; //temp code until images are added
+        break;
+    }
+    InputDiv.appendChild(instructionSymbol);
+    return InputDiv;
+  }
+}
 
 document.querySelector('.instruction > .XIC').addEventListener('click', addXIC);
 
 function addXIC() {
-  test = new XIC();
-  document.querySelector('.Rung').innerText += test.structuredText;
+  test = new inputDiv('XIC');
+  // alert(test.addedInstruction.structuredText);
+  document.querySelector('.Rung').appendChild(test);
 }
 
+const newrungbutton = document.querySelector(".NewRung");
 newrungbutton.addEventListener('click', addnewrung);
 
 function addnewrung() {
-  const addedrung = document.createElement('p');
+  const addedrung = document.createElement('div');
   addedrung.className = 'Rung';
-  addedrung.innerText = `${NumOfRungs}`;
+  const rungnumber = document.createElement('div');
+  rungnumber.className = 'RungNumber';
+  rungnumber.innerText = NumOfRungs;
+  addedrung.appendChild(rungnumber);
   document.querySelector('.Code').appendChild(addedrung);
   NumOfRungs ++;
 }
